a6861677b701147dc4fee1a8b4a0fc8774dfeac0
Harun-Ar- Rashid <suadabdullahal@gmail.com>
2021-08-07 18:40:56 +0600

    react route done

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
     <>
              
         <Router>
-        
+        <Navbar/>
             <Switch>
                 <Route exact path="/" component={Home}/>
                 <Route exact path="/about" component={About}/>
